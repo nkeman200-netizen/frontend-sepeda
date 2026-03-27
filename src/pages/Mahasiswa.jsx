ec53a163f401f334a65b7535a55a0548928013a4
Sofyan <nkeman200@gmail.com>
2026-03-27 17:08:17 +0700

    riwayat user+biar otomatis

--- src/pages/Mahasiswa.jsx
+++ src/pages/Mahasiswa.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom'; // 👈 PASTIKAN INI ADA
 import Swal from 'sweetalert2'
 import api from '../api';
 import { list } from 'postcss';
+import TabelRiwayat from '../components/TabelRiwayat';
 
 function Mahasiswa() {
   // 1. DEKLARASI STATE (Ingatan React)
@@ -254,39 +255,7 @@ function Mahasiswa() {
                 )
             ) : ( 
                 /* TAB 2: AREA TABEL RIWAYAT */
-                <div className="bg-white p-6 rounded-lg shadow-md mb-8">
-                    <h2 className="text-xl font-bold text-gray-800 mb-4">Riwayat Transaksi {localStorage.getItem('USERNAME')}</h2>
-                    <div className="overflow-x-auto">
-                        <table className="min-w-full text-left border-collapse">
-                            <thead className="bg-gray-800 text-white">
-                                <tr>
-                                    <th className="py-3 px-4 rounded-tl-lg font-semibold">Sepeda</th>
-                                    <th className="py-3 px-4 font-semibold">Durasi (jam)</th>
-                                    <th className="py-3 px-4 font-semibold">Waktu Pinjam</th>
-                                    <th className="py-3 px-4 rounded-tr-lg font-semibold">Waktu Kembali</th>
-                                </tr>
-                            </thead>
-                            <tbody className="text-gray-700">
-                                {riwayatList.length === 0 ? (
-                                    <tr className="border-b border-gray-200 hover:bg-gray-50 transition duration-150">
-                                        <td colSpan="3" className="py-3 px-4 text-center">Data Kosong</td>
-                                    </tr>
-                                ) : (                   
-                                    riwayatList.map((item) => (
-                                        <tr key={item.idPinjam} className="border-b border-gray-200 hover:bg-gray-50 transition duration-150">
-                                            <td className="py-3 px-4">{item.merkSepeda}</td>
-                                            <td className="py-3 px-4">{item.durasi}</td>
-                                            <td className="py-3 px-4">{new Date(item.waktuPinjam).toLocaleString('id-ID')}</td>
-                                            <td className="py-3 px-4">
-                                                {item.waktuKembali ? new Date(item.waktuKembali).toLocaleString('id-ID') : "Belum dikembalikan"}
-                                            </td>
-                                        </tr>
-                                    ))
-                                )}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                <TabelRiwayat riwayatList={riwayatList}/>
             )}
             {/* ==== SELESAI SIHIR TAB ==== */}
         </div>

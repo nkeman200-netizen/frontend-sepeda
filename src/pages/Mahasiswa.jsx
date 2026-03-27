9018876a996f62f5b008ba16e764993513d1243f
Sofyan <nkeman200@gmail.com>
2026-03-27 14:21:12 +0700

    riwayat user+biar otomatis

--- src/pages/Mahasiswa.jsx
+++ src/pages/Mahasiswa.jsx
@@ -179,7 +179,6 @@ function Mahasiswa() {
                             <thead className="bg-gray-800 text-white">
                                 <tr>
                                     <th className="py-3 px-4 rounded-tl-lg font-semibold">Sepeda</th>
-                                    <th className="py-3 px-4 font-semibold">Peminjam</th>
                                     <th className="py-3 px-4 font-semibold">Waktu Pinjam</th>
                                     <th className="py-3 px-4 rounded-tr-lg font-semibold">Waktu Kembali</th>
                                 </tr>
@@ -193,7 +192,6 @@ function Mahasiswa() {
                                 riwayatList.map((item) => (
                                 <tr key={item.idPinjam} className="border-b border-gray-200 hover:bg-gray-50 transition duration-150">
                                     <td className="py-3 px-4">{item.merkSepeda}</td>
-                                    <td className="py-3 px-4">{item.username}</td>
                                     <td className="py-3 px-4">{new Date(item.waktuPinjam).toLocaleString('id-ID')}</td>
                                     <td className="py-3 px-4">{
                                         item.waktuKembali?(new Date(item.waktuKembali).toLocaleString('id-ID')) 
